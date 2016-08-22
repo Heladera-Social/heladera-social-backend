c91cd9ac88b00b944e6d5dc268c59c1b7fb1db45
Nacho Rivera <rivieng@gmail.com>
2016-08-21 22:51:35 -0300

    user, device, active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HeladeraSocialBackend::Application.routes.draw do
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root to: 'application#index'
 
   require 'sidekiq/web'
