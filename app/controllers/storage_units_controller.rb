a86bae4b0d34cbf85135a1c1f0321bf47e58221d
Nacho Rivera <rivieng@gmail.com>
2016-09-24 19:39:06 -0300

    list unexpird products

--- app/controllers/storage_units_controller.rb
+++ app/controllers/storage_units_controller.rb
@@ -14,7 +14,7 @@ class StorageUnitsController < ApplicationController
   def show
     @products = []
     current_user.storage_units.each do |s|
-      s.products.each do |p|
+      s.products.unexpired.each do |p|
         @products << p
       end
     end
