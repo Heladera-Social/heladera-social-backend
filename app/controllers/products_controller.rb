7d7d4edb19e45a7b64f58b557003ff54f022e576
Nacho Rivera <rivieng@gmail.com>
2016-09-24 15:57:07 -0300

    donations search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   def index
   	@products = Product.where(product_type: params[:product_type]).uniq if params[:product_type].present?
     @products = Product.all.uniq unless params[:product_type].present?
+    @storage_units = current_user.storage_units.all
     @products_types = ProductType.all
   end
   
