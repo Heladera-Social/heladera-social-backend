3f898bf1e5d3c92e901af46a712a2e060283b681
kevin kraus <kevinkraus92@hotmail.com>
2017-01-22 19:26:58 -0300

    Added labels to filters when they were to expire & fixed filters

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,5 +14,5 @@ class Product < ActiveRecord::Base
   scope :expired, -> { where('expiration_date < ? OR expiration_date IS NULL', Time.zone.today) }
   scope :unexpired, -> { where('expiration_date >= ? OR expiration_date IS NULL', Time.zone.today) }
   scope :available, -> { where('quantity > 0').unexpired }
-  scope :expires_in, ->(days) { where('expiration_date = ? OR expiration_date IS NULL', Time.zone.today + days.days) }
+  scope :expires_in, ->(days) { where('expiration_date <= ? AND expiration_date >= ? OR expiration_date IS NULL', Time.zone.today + days.days, Time.zone.today) }
 end
